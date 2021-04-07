79b0fb3eb8c0cfc5c7d3fdc935931e570d1dbcb1
kneazle <kneazle@yandex.ru>
2021-04-08 02:55:53 +0300

    Task 0

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("Fight...");
+const player1 = {
+  name: "Tom",
+  img: "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif",
+  weapon: "копье",
+  attack: function () {
+    console.log(this.name + " Fight...");
+  },
+};
+
+const player2 = {
+  name: "Harry",
+  img: "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif",
+  weapon: "wand",
+  attack: function () {
+    console.log(this.name + " Fight...");
+  },
+};
+
+player1.attack();
+player2.attack();
