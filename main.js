5352b7621437ed58c4690f5517c92bc3e15c56b5
kneazle <kneazle@yandex.ru>
2021-04-11 02:51:00 +0300

    Task 0

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+const $arena = document.querySelector(".arenas");
+
 const player1 = {
+  player: 1,
   name: "Tom",
-  hp: 80,
+  hp: 100,
   img: "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif",
   weapon: ["копье"],
   attack: function () {
@@ -9,8 +12,9 @@ const player1 = {
 };
 
 const player2 = {
+  player: 2,
   name: "Harry",
-  hp: 15,
+  hp: 100,
   img: "http://reactmarathon-api.herokuapp.com/assets/liukang.gif",
   weapon: ["wand"],
   attack: function () {
@@ -18,37 +22,33 @@ const player2 = {
   },
 };
 
-function createPlayer(playerClass, playerObject) {
-  const $arena = document.querySelector(".arenas");
+function createElement(tag, className) {
+  const $tag = document.createElement(tag);
+  if (className) $tag.classList.add(className);
 
-  const $player = document.createElement("div");
-  $player.classList.add(playerClass);
+  return $tag;
+}
 
-  const $progressbar = document.createElement("div");
-  $progressbar.classList.add("progressbar");
+function createPlayer(playerObject) {
+  const $player = createElement("div", "player" + playerObject.player);
+  const $progressbar = createElement("div", "progressbar");
+  const $life = createElement("div", "life");
+  const $name = createElement("div", "name");
+  const $character = createElement("div", "character");
+  const $img = createElement("img");
 
-  const $life = document.createElement("div");
-  $life.classList.add("life");
   $life.style.width = playerObject.hp + "%";
-
-  const $name = document.createElement("div");
-  $name.classList.add("name");
   $name.innerText = playerObject.name;
+  $img.src = playerObject.img;
+
   $progressbar.appendChild($life);
   $progressbar.appendChild($name);
-
-  const $character = document.createElement("div");
-  $character.classList.add("character");
-
-  const $img = document.createElement("img");
-  $img.src = playerObject.img;
   $character.appendChild($img);
-
   $player.appendChild($progressbar);
   $player.appendChild($character);
 
-  $arena.appendChild($player);
+  return $player;
 }
 
-createPlayer("player1", player1);
-createPlayer("player2", player2);
+$arena.appendChild(createPlayer(player1));
+$arena.appendChild(createPlayer(player2));
